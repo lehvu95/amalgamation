7c6f3c74d3d78879fec6d345df1b22e1ab425862
lehvu95 <lehvu95@gmail.com>
2021-07-07 00:16:50 -0500

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Home';
 
-function App() {
+const URL_ARRAY = [
+  {
+    label: 'Home',
+    url: '/',
+  },
+  {
+    label: 'Medication List',
+    url: '/medication-list',
+  },
+  {
+    label: 'Checkbook Register',
+    url: '/checkbook-register',
+  },
+  {
+    label: 'Minesweeper',
+    url: '/minesweeper',
+  },
+]
+
+const App = () => {
+  const getNav = () => {
+    return (
+      <table>
+        <tr>
+          {URL_ARRAY.map((item) => {
+            const {label, url} = item;
+            return <td><Link to={url}>{label}</Link></td>;
+          })}
+        </tr>
+      </table>
+    );
+  }
+
+  
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        an Amalgamation of coding challenges
+        {getNav()}
       </header>
+      <Switch>
+        <Route path='/'>
+          <Home/>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
